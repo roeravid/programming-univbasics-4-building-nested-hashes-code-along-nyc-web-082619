4c67bff2ef5da1b318cbfb4d18666e6da305f3e6
Joe David <javid92@gmail.com>
2019-08-22 07:03:41 +0000

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,7 +4,19 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
+      :hero_friends =>
+      [
+       {
+        :name => "benvolio",
+        :age => "17",
+        :attitude => "worried",
+       }.
+       
+       {
+        :name => "mercutio",
+        :age => "18",
+        :attitude => "hot-headed"
+       }
         #your hashes here!
       ]
    }, 
@@ -13,6 +25,17 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
+        {
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        },
+        
+        {
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        }
         #your hashes here!
       ]
    }
