194e2a8959fc9c97a59be09e0d7aeb5ac33503e4
Joe David <javid92@gmail.com>
2019-08-22 06:54:03 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,12 +2,19 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Montague"
+        :age => "53"
+        #your key/alue pairs here
         },
       :matriarch => {
+        :name => "Lady Montague"
+        :age => "54" 
         #your key/value pairs here
         },
       :hero => {
+        :name => "Romeo"
+        :age => "15"
+        :status => "alive"
         #your key/value pairs here
         },
       :hero_friends => []
